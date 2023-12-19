88264dae419ba59e8022e003fd2050256567f2fc
ZengChao <42760076+Neekky@users.noreply.github.com>
2023-12-19 13:59:34 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 const path = require("path");
 
-const rootPath = path.join(__dirname, '../stock_data_base/data')
+const rootPath = path.join(__dirname, '../../stock_data_base/data')
 
-const crawlPath = path.join(__dirname, '../stock_analyse_tool_data_crawl/database');
+const crawlPath = path.join(__dirname, '../../stock_analyse_tool_data_crawl/database');
 
 module.exports = {
     rootPath,
